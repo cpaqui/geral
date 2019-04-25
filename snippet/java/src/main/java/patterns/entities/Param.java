8e2433150beee8afb125dda304e60488fc100a0a
cleive.paqui <cleive.paqui@inm.pt>
2019-04-25 10:48:18 +0100

    Param classe.

--- snippet/java/src/main/java/patterns/entities/Param.java
+++ snippet/java/src/main/java/patterns/entities/Param.java
@@ -4,17 +4,17 @@ import java.util.Optional;
 
 public class Param<T> {
 
-    private Optional<T> data;
+    private T data;
 
-    public Param(Optional<T> data) {
+    public Param(T data) {
         this.data = data;
     }
 
     public Optional<T> getData() {
-        return data;
+        return Optional.of(data);
     }
 
-    public void setData(Optional<T> data) {
+    public void setData(T data) {
         this.data = data;
     }
 }
